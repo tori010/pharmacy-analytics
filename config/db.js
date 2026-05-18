73fa9f52c0c35ebed8add20fb75376e9a528002f
Omar-Sameh-fouad <omarsamehfouad1@gmail.com>
2026-05-18 23:57:46 +0300

    fix db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456', 
-  database: process.env.DB_NAME || 'careplus_db',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10
 };
@@ -12,3 +13,10 @@ const dbConfig = {
 const pool = mysql.createPool(dbConfig);
 
 module.exports = pool;
+
+/*host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456', 
+  database: process.env.DB_NAME || 'careplus_db',
+  waitForConnections: true,
+  connectionLimit: 10*/

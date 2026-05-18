28a2006885562b180ab914ca3a20cdef80c8c1b5
Omar-Sameh-fouad <omarsamehfouad1@gmail.com>
2026-05-19 01:19:44 +0300

    fix db

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+
 require('dotenv').config();
+
+// مؤقت للـ debugging
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
